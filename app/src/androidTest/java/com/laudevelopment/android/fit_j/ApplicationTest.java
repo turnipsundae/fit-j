15c2b0f86ba3767a8b4267c771053d4c7060eec7
Kevin Lau <laudevelopment@gmail.com>
2016-07-28 23:09:43 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/laudevelopment/android/fit_j/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.laudevelopment.android.fit_j;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
